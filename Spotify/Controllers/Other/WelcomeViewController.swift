6018e3cb5d0fb51ba3a7cb02b2906d6732dbe3d4
Niff1e <malanka.kering@gmail.com>
2025-01-12 14:52:01 +0300

    Appearence of welcome screen changed

--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -14,27 +14,54 @@ class WelcomeViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .white
         button.setTitle("Sign In with Spotify", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 10
         return button
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "screen")
+        return imageView
+    }()
+    
+    private let overlayView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        view.alpha = 0.7
+        return view
+    }()
+    
+    private let centerLogoView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "welcome_screen_logo")
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        label.text = "Listen to Millions\nof Songs on\nto go"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Spotify"
-        view.backgroundColor = .systemGreen
-        view.addSubview(signInButton)
+        view.backgroundColor = .blue
+        positionOfSubviews()
+        
         signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        signInButton.frame = CGRect(
-            x: 20,
-            y: view.height - 50 - view.safeAreaInsets.bottom,
-            width: view.width - 40,
-            height: 50
-        )
     }
     
     @objc func didTapSignIn() {
@@ -62,5 +89,42 @@ class WelcomeViewController: UIViewController {
         let mainAppTabBarVC = TabBarViewController()
         mainAppTabBarVC.modalPresentationStyle = .fullScreen
         present(mainAppTabBarVC, animated: true)
+    }
+    
+    private func positionOfSubviews() {
+        view.addSubview(imageView)
+        view.addSubview(overlayView)
+        view.addSubview(centerLogoView)
+        view.addSubview(label)
+        view.addSubview(signInButton)
+        
+        let imageSize = view.width/3
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: self.view.heightAnchor),
+            
+            overlayView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            overlayView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            overlayView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            overlayView.heightAnchor.constraint(equalTo: self.view.heightAnchor),
+            
+            centerLogoView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            centerLogoView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -20.0),
+            centerLogoView.widthAnchor.constraint(equalToConstant: imageSize),
+            centerLogoView.heightAnchor.constraint(equalToConstant: imageSize),
+            
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: centerLogoView.bottomAnchor, constant: 30.0),
+            label.widthAnchor.constraint(equalToConstant: view.width - 60.0),
+            label.heightAnchor.constraint(equalToConstant: 150.0),
+            
+            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20.0),
+            signInButton.widthAnchor.constraint(equalToConstant: view.width - 80.0),
+            signInButton.heightAnchor.constraint(equalToConstant: 60.0)
+        ])
     }
 }
